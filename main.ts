f82edd8871c425a82893322d0a6a661c0a26d788
Melkor-Morg0th <atwright55@gmail.com>
2025-02-24 14:47:22 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorOpenSouth, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level4`)
+    tiles.placeOnRandomTile(mySprite, assets.tile`myTile8`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, location) {
+    game.setGameOverEffect(false, effects.none)
+    music.play(music.tonePlayable(233, music.beat(BeatFraction.Double)), music.PlaybackMode.LoopingInBackground)
+    game.gameOver(false)
+})
+let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level2`)
-let mySprite = sprites.create(assets.image`main character`, SpriteKind.Player)
+mySprite = sprites.create(assets.image`main character`, SpriteKind.Player)
 tiles.placeOnRandomTile(mySprite, sprites.castle.tilePath5)
 scene.cameraFollowSprite(mySprite)
 controller.moveSprite(mySprite)
+mySprite.setScale(0.3, ScaleAnchor.Middle)
