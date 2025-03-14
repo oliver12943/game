403446e5eda7ac5ca1df285b405ed4c0f9c24da9
Melkor-Morg0th <atwright55@gmail.com>
2025-03-14 14:52:21 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -4,12 +4,18 @@ namespace SpriteKind {
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorOpenSouth, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level4`)
     tiles.placeOnRandomTile(mySprite, assets.tile`myTile8`)
+    mySprite2 = sprites.create(assets.image`myImage1`, SpriteKind.Artefact)
+    tiles.placeOnRandomTile(mySprite2, assets.tile`myTile14`)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, location) {
     game.setGameOverEffect(false, effects.none)
     music.play(music.tonePlayable(233, music.beat(BeatFraction.Double)), music.PlaybackMode.LoopingInBackground)
     game.gameOver(false)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Artefact, function (sprite, otherSprite) {
+    game.gameOver(true)
+})
+let mySprite2: Sprite = null
 let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level2`)
 mySprite = sprites.create(assets.image`main character`, SpriteKind.Player)
